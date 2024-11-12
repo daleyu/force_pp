f4eba3e28104c0370b93fb8e99382d1aa0415622
daleyu <yudale2003@gmail.com>
2024-11-12 01:37:35 -0500

    update the token class to use enum for tokentype

--- token/token.h
+++ token/token.h
@@ -1,16 +1,78 @@
-// token.h
-
 #ifndef TOKEN_H
 #define TOKEN_H
 
 #include <string>
 #include <unordered_map>
 #include <vector>
+
+enum class TokenType {
+    ILLEGAL,
+    EOF_TOKEN,
+
+    // Identifiers + literals
+    IDENT,      // add, foobar, x, y, ...
+    INT_LITERAL,
+    FLOAT_LITERAL,
+    STRING_LITERAL,
+    CHAR_LITERAL,
+    BOOLEAN_LITERAL,
+
+    // Operators
+    ASSIGN,
+    PLUS,
+    MINUS,
+    BANG,
+    ASTERISK,
+    SLASH,
+
+    LT,
+    GT,
+    LTE,
+    GTE,
+    AND,
+    OR,
+    NOT_EQ,
+    EQ,
+
+    // Delimiters
+    COMMA,
+    SEMICOLON,
+    COLON,
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+    LBRACKET,
+    RBRACKET,
+
+    // Keywords
+    FUNCTION,
+    LET,
+    TRUE,
+    FALSE,
+    IF,
+    ELSE,
+    RETURN,
+    INT,
+    FLOAT,
+    CHAR,
+    BOOL,
+    VARCHAR,
+    VI,
+    FOR,
+    WHILE,
+
+    VOID,
+};
+
+std::string TokenTypeToString(TokenType type);
+
 class Token {
 public:
-    std::string type;
+    TokenType type;
     std::string literal;
 
-    Token(const std::string type, const std::string& literal) : type(type), literal(literal) {}
+    Token(TokenType type, const std::string& literal) : type(type), literal(literal) {}
 };
+
 #endif // TOKEN_H
